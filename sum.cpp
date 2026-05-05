4202ab87eac578beae95b64bcc3e0ba401b2192a
Abhinav-Bathala <bathala.abhinav@gmail.com>
2026-05-04 20:43:41 -0400

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+
+
+int main(){
+    int a, b;
+    cout << "Enter a number: "; 
+    cin >> a; 
+    cout << "Enter a number: "; 
+    cin >> b; 
+
+
+    cout << a + b;
+
+
+}
